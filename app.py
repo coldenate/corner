531add1460c2edf03e33e9f41e1c7e3b4a9ba8de
colderinit <n8sol022@gmail.com>
2021-05-16 13:51:35 -0500

    added le app.py

---
+++ app.py
@@ -0,0 +1,112 @@
+import random
+import numpy as np
+import pickle
+import json
+import os
+from flask import Flask, render_template, request
+from flask_ngrok import run_with_ngrok
+import nltk
+from keras.models import load_model
+from nltk.stem import WordNetLemmatizer
+print('imports are done')
+lemmatizer = WordNetLemmatizer()
+print('lemmatizer is inititntintintitn')
+
+# dirname = os.path.dirname(__file__)
+# data_filename = os.path.join(dirname, 'intents.json')
+
+# file path inits because my fp system is broken
+# ----------------------------------------------
+dirname = os.path.dirname(__file__)  # This can be used throughout the file
+modelpath = os.path.join(dirname, 'chatbot_model.h5')
+intentspath = os.path.join(dirname, 'intents.json')
+wordspath = os.path.join(dirname, 'words.pkl')
+classespath = os.path.join(dirname, 'classes.pkl')
+# ----------------------------------------------
+
+# init chat
+model = load_model(modelpath)
+intents = json.loads(open(intentspath).read())
+words = pickle.load(open(wordspath, "rb"))
+classes = pickle.load(open(classespath, "rb"))
+
+app = Flask(__name__)
+# run_with_ngrok(app) -Use this option if you have ngrok and you want to expose your chatbot to the real world
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get", methods=["POST"])
+def chatbot_response():
+    msg = request.form["msg"]
+    # checks is a user has given a name, in order to give a personalized feedback
+    if msg.startswith('my name is'):
+        name = msg[11:]
+        ints = predict_class(msg, model)
+        res1 = getResponse(ints, intents)
+        res = res1.replace("{n}", name)
+    elif msg.startswith('hi my name is'):
+        name = msg[14:]
+        ints = predict_class(msg, model)
+        res1 = getResponse(ints, intents)
+        res = res1.replace("{n}", name)
+    # if no name is passed execute normally
+    else:
+        ints = predict_class(msg, model)
+        res = getResponse(ints, intents)
+    return res
+
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(
+        word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return the bag of words in an array : 0 or 1 for each word in the bag that exists inside the sentence
+
+
+def bow(sentence, words, show_details=True):
+    # tokenize patter
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words | matrix of words, vocab matrix
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                # assign 1 if current word is in the vocab position
+                bag[i] = 1
+                if show_details:
+                    print("Found in the bag: %s" % w)
+    return np.array(bag)
+
+
+def predict_class(sentence, model):
+    # filter out predictions below a set threshold
+    p = bow(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    # sort by stronk of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+
+def getResponse(ints, intents_json):
+    tag = ints[0]["intent"]
+    list_of_intents = intents_json["intents"]
+    for i in list_of_intents:
+        if i["tag"] == tag:
+            result = random.choice(i["responses"])
+            break
+    return result
+
+
+if __name__ == "__main__":
+    app.run()
